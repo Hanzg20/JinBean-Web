960bf34ef4557d3dd03cb25d0d69a497aa0e8265
Andy Han <andyhan@example.com>
2025-06-28 19:07:06 -0400

    fix: remove unused 'Spotlight' import in instrumentation-client to fix static export build error

--- apps/web/src/instrumentation-client.ts
+++ apps/web/src/instrumentation-client.ts
@@ -2,7 +2,6 @@
 // The added config here will be used whenever a users loads a page in their browser.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 // import * as Sentry from '@sentry/nextjs';
-import * as Spotlight from '@spotlightjs/spotlight';
 
 // if (!process.env.NEXT_PUBLIC_SENTRY_DISABLED) {
 //   Sentry.init({
