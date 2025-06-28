9056607f47ce38718fe795cd1bb03d78025faa29
Andy Han <andyhan@example.com>
2025-06-28 18:25:33 -0400

    fix: add empty db export for static export debug

--- apps/web/src/libs/DB.ts
+++ apps/web/src/libs/DB.ts
@@ -2,3 +2,4 @@
 // import { drizzle } from 'drizzle-orm/node-postgres';
 // import { migrate } from 'drizzle-orm/node-postgres/migrator';
 // export const getDb = () => null;
+export const db = undefined;
